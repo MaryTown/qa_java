ff168c0d065d502277fb5ca9ecd6872c955aae15
Mary <68908592+MaryTown@users.noreply.github.com>
2023-08-13 23:39:04 +0300

    Update FelineTest.java

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -2,17 +2,12 @@ import com.example.Feline;
 import org.junit.Test;
 import java.util.List;
 import org.junit.Assert;
-import org.junit.Before;
 
 public class FelineTest {
 
-    @Before
-    public void initFeline() {
-        Feline feline = new Feline();
-    }
-
     @Test
     public void felineEatMeatTest() throws Exception {
+        Feline feline = new Feline();
         List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
         List<String> actualResult = feline.eatMeat();
         Assert.assertEquals(expectedResult, actualResult);
@@ -20,6 +15,7 @@ public class FelineTest {
 
     @Test
     public void felineGetFamilyTest() {
+        Feline feline = new Feline();
         String expectedResult = "Кошачьи";
         String actualResult = feline.getFamily();
         Assert.assertEquals(expectedResult, actualResult);
@@ -27,6 +23,7 @@ public class FelineTest {
 
     @Test
     public void felineGetKittensTest() {
+        Feline feline = new Feline();
         int expectedResult = 1;
         int actualResult = feline.getKittens();
         Assert.assertEquals(expectedResult, actualResult);
@@ -34,7 +31,8 @@ public class FelineTest {
 
     @Test
     public void felineGetKittensCountTest() {
-        int expected = 10;
+        Feline feline = new Feline();
+        int expectedResult = 10;
         int actualResult = feline.getKittens(10);
         Assert.assertEquals(expectedResult, actualResult);
     }
